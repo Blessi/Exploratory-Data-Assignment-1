40b4c8ecc09451cf21138cd5dc187a72d3c64e97
Blessing <boriaifo@yahoo.co.uk>
2016-06-23 18:25:47 +0100

    Fourth commit

--- plot2.R
+++ plot2.R
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #Getting full dataset
 data_full <- read.csv("./household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
@@ -17,23 +17,3 @@ plot(data$Global_active_power~data$Datetime, type="l",
      ylab="Global Active Power (kilowatts)", xlab="")
 dev.copy(png, file="plot2.png", height=480, width=480)
 dev.off()
-=======
-#Getting full dataset
-data_full <- read.csv("./household_power_consumption.txt", header=T, sep=';', na.strings="?", 
-                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
-
-# Subsetting the data
-data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(data_full)
-
-# Converting dates
-datetime <- paste(as.Date(data$Date), data$Time)
-data$Datetime <- as.POSIXct(datetime)
-
-# Plot 2
-plot(data$Global_active_power~data$Datetime, type="l",
-     ylab="Global Active Power (kilowatts)", xlab="")
-dev.copy(png, file="plot2.png", height=480, width=480)
-dev.off()
->>>>>>> 647fc82635e82c6e20d60080f1d2ec17e778dd62
